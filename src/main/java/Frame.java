4d6a6abef1363276bcd5d9eb1e543774ef1d1567
Robert Levenbach <R.W.Levenbach@student.tudelft.nl>
2018-04-25 21:54:52 +0200

    Added a button

--- src/main/java/Frame.java
+++ src/main/java/Frame.java
@@ -3,9 +3,12 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+// JAVAFX: https://docs.oracle.com/javafx/2/get_started/jfxpub-get_started.html
+//https://docs.oracle.com/javafx/2/overview/jfxpub-overview.html
 public class Frame  extends Application {
 
     public static void main(String[] args) {
@@ -14,8 +17,20 @@ public class Frame  extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello World!");
+        primaryStage.setTitle("DSR-C!");
         Button btn = new Button();
+
+        Button btn2 = new Button();
+
+        btn2.setText("Fck Bitches");
+        btn2.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Fck Bitches");
+            }
+        });
+
         btn.setText("Say 'Hello World'");
         btn.setOnAction(new EventHandler<ActionEvent>() {
 
@@ -25,9 +40,19 @@ public class Frame  extends Application {
             }
         });
 
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
+        Pane root = new Pane();
+        //Set Frame
+        primaryStage.setScene(new Scene(root, 500, 250));
         primaryStage.show();
+        //Add button1
+        btn.setLayoutX(0);
+        btn.setLayoutY(primaryStage.getMaxHeight());
+        root.getChildren().add(btn);
+        //Add button2
+        btn2.setLayoutX(0);
+        btn2.setLayoutY(0);
+        root.getChildren().add(btn2);
+
+
     }
 }
