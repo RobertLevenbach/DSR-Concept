7b4db30f6066ad68ca9691c5a0627560012828ec
Robert Levenbach <R.W.Levenbach@student.tudelft.nl>
2018-04-25 21:36:15 +0200

    first commit

---
+++ src/main/java/Launcher.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Launcher {
+
+    public static void main(String[] args)
+    {
+        new Frame().main(args);
+    }
+
+    private void launch() {
+
+
+    }
+
+
+
+}
